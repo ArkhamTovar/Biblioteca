5c9924e0e067ac931c4d3c4ffa45717d99ccc433
Arkham Tovar <tovararkham@gmail.com>
2023-03-04 18:06:48 -0600

    Subida de programa a HUB

---
+++ biblioteca.py
@@ -0,0 +1,195 @@
+#Diccionario en el que se almacenaran los libros
+libros=dict()
+
+def RegistrarNuevoEjempar():
+    while True:
+        global identificador
+        print("")
+        print("****Registrar Libro*****")
+        #Generacion de llaves de diccionario; si ya hay llaves en el diccionario añade 1 mas al conteo, si no, asigna la primera lista primero con el numero 1
+        if libros.keys():
+            identificador=max(libros.keys())+1
+        else:
+            identificador=1
+        print()
+        
+        #Entrada de datos para el registro de libro
+        titulo=input("Ingresa el titulo: ")
+        titulo=titulo.upper()
+        
+        autor=input("Ingresa el autor: ")
+        autor=autor.upper()
+        
+        genero=input("Ingresa el genro: ")
+        genero=genero.upper()
+        
+        añoPublic=input("Ingresa el año de publicacion: ")
+        
+        isbn=input("Ingresa el ISBN: ")
+        isbn=isbn.upper()
+
+        fechAdqui=input("Ingresa el año de adquisicion: ")
+        
+        #Datos ingresados almacenados en la lista "ejemplar"
+        ejemplar=(titulo,autor,genero,añoPublic,isbn,fechAdqui)
+        
+        #Lista ejemplar guardada en el diccionario de "libros"
+        libros[identificador]=ejemplar
+        
+        
+        agregar=input("Desea agregar otro libro? [S/N]: ")
+        agregar=agregar.upper()
+        
+        if agregar=="S":
+            pass
+        elif agregar=="N":
+            break
+        else:
+            print("opcion no valida")
+
+
+#Funcion que muestra todo el catalago completo de libros
+def MostrarCatalagoCompleto():
+    print()
+    print("*******Catalago completo********")
+    print("TITULO | AUTOR | GENERO | AÑO PUBLICACION | ISBN | AÑO ADQUISICION")
+    for libro in libros.values(): #Busqueda de los datos por medio de pocisiones en la lista
+        print(f"{libro[0]} | {libro[1]} | {libro[2]} | {libro[3]} | {libro[4]} | {libro[5]}")
+
+#Funcion debusqueda por autor
+def ReportePorAutor():
+    autores=list()
+    print()
+    print("*******Reporte por autor******")
+    
+    #Muestra todos los autores de libros sin repetirlos
+    autores=list()
+    for libro in libros.values():
+        autores.append(libro[1])
+        for autor in autores:
+            if autores.count(autor)>1:
+                autores.remove(autor)
+    print("---AUTORES DISPONIBLES----")
+    for autor in autores:
+        print(f"-{autor}")
+    
+    #Consulta
+    autor=input("Ingrese el nombre del autor: ")
+    autorBuscado=autor.upper()
+    try:
+        print()
+        print("TITULO | AUTOR | GENERO | AÑO PUBLICACION | ISBN | AÑO ADQUISICION")
+        for libro in libros.values():
+            if libro[1]==autorBuscado:
+                print(f"{libro[0]} | {libro[1]} | {libro[2]} | {libro[3]} | {libro[4]} | {libro[5]}")
+    except:
+        pass
+
+# Sub menu de Reportes
+def Reportes():
+    print()
+    print("*****Reportes*****")
+    print("*1* Catalago completo")
+    print("*2* Reporte por autor")
+    eleccion=int(input("Selecciona una opcion: "))
+    if eleccion==1:
+        MostrarCatalagoCompleto()
+    if eleccion==2:
+        ReportePorAutor()
+
+
+
+def BusquedaPorTitulo():
+    print()
+    print("*****Busqueda por titulo******")
+    titulos=list()
+    for libro in libros.values():
+        titulos.append(libro[0])
+        for titulo in titulos:
+            if titulos.count(titulo)>1:
+                titulos.remove(titulo)
+    print("-----TITULOS DISPONIBLES-----")
+    for titulo in titulos:
+        print(f"-{titulo}")
+    #Consulta
+    titulo=input("Ingrese el titulo del libro: ")
+    tituloBuscado=titulo.upper()
+    try:
+        print()
+        print("TITULO | AUTOR | GENERO | AÑO PUBLICACION | ISBN | AÑO ADQUISICION")
+        for libro in libros.values():
+            if libro[0]==tituloBuscado:
+                print(f"{libro[0]} | {libro[1]} | {libro[2]} | {libro[3]} | {libro[4]} | {libro[5]}")
+    except:
+        pass
+
+
+def BusquedaPorISBN():
+    print()
+    print("*****Busqueda por ISBN******")
+
+    #Consulta
+    isbn=input("Ingrese el ISBN del libro: ")
+    try:
+        print()
+        print("TITULO | AUTOR | GENERO | AÑO PUBLICACION | ISBN | AÑO ADQUISICION")
+        for libro in libros.values():
+            if libro[4]==isbn:
+                print(f"{libro[0]} | {libro[1]} | {libro[2]} | {libro[3]} | {libro[4]} | {libro[5]}")
+    except:
+        pass
+
+#Sub menu para la busqueda por titulo y ISBN
+def TituloYIsbn():
+    while True:
+        print()
+        print("**********Consulta por titulo y ISBN****")
+        print()
+        print("*1* busqueda por Titulo")
+        print("*2* Busqueda por ISBN")
+        print("*3* Volver al menu principal")
+        eleccion=int(input("Elige una opcion: "))
+        if eleccion==1:
+            BusquedaPorTitulo()
+        if eleccion==2:
+            BusquedaPorISBN()
+        if eleccion==3:
+            break
+
+#Submenu de consultas y reportes 
+def ConsultaYReportes():
+    while True:
+        print()
+        print("*********CONSULTA Y REPORTES****************")
+        
+        print("*1* Consulta de titulo y ISBN")
+        print("*2* Reportes")
+        print("*3* Volver al menu principal")
+        eleccion=int(input("Elige una opcion: "))
+        if eleccion==1:
+            TituloYIsbn()
+        if eleccion==2:
+            Reportes()
+        if eleccion==3:
+            break
+
+# Menu principal
+def Menu():
+    while True:
+        print()
+        print("***********BIBLIOTECA*************")
+        print()
+        print("*1* Registrar nuevo ejemplar")
+        print("*2* Consultas y reportes")
+        print("*3* Salir")
+        print()
+        var_elect=input("Ingrese un numero: ")
+        if var_elect=="1":
+            RegistrarNuevoEjempar()
+        if var_elect=="2":
+            ConsultaYReportes()
+        elif var_elect=="3":
+            print("Ha salido del programa")
+            break
+
+Menu()
